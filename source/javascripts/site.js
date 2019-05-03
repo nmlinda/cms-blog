119221773f0e8736e679ca4f99ddedfa92805770
Nur Meilinda Nugraha <nmlinda97@gmail.com>
2019-05-03 10:46:22 +0700

    add bulma

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,3 @@
 // This is where it all goes :)
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css"></link>
+<script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
